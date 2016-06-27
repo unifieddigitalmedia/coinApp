08d058a2f3ccf1b6a87581e15af05d8cf826752f
unifieddigitalmedia <Home@Machels-MacBook-Pro.local>
2016-06-27 21:32:44 +0100

    Automated tests
    The scripts contained within the test folder use Jasmine.js to carry
    out behavior-driven development  testing of JavaScript code that
    accepts an user’s input and validates whether the desired integer is
    expressed as a pence or pound value

---
+++ test/testSuites.js
@@ -0,0 +1,34 @@
+describe('Coin App Tests',function() {
+  
+  
+it("should validate that amount entered is a pence value", function() {
+
+var amount = ".23p" ;
+
+  var pencePattern = /(^[.]\d+[p]$)|(^\d+[p]$)|(^[.]\d+$)/g;
+  
+
+    expect(pencePattern.test(amount)).toBe(true);
+  
+
+  });
+
+
+it("should validate that amount entered is a pound value", function() {
+
+
+
+  var amount = "1.23p" ;
+
+  var poundPattern = /(^\d+[.]\d+$)|(^\d+$)|(^[£]\d+[.]\d+[p]$)|(^[£]\d+[.][p]$)|(^[£]\d+[.]\d+$)|(^[£]\d+$)|(^\d+[.][p]$)|(^[£]\d+[.]$)|(^0+\d+[.]\d+$)|(^0+\d+[.]\d+[p]$)|(^\d+[.]\d+[p]$)/g;
+  
+
+    expect(poundPattern.test(amount)).toBe(true);
+  
+
+  });
+
+
+ 
+  
+});
